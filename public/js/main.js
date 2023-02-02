d5b061bd7b40d3a77ee826aeb0a032efda58291c
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-02-02 23:24:33 +0630

    fetch api

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,39 @@ form.addEventListener("submit", function (e) {
     return;
   }
 
-  console.log(prompt, size);
+  generateImageRequest(prompt, size);
 });
+
+async function generateImageRequest(prompt, size) {
+  try {
+    showSpinner();
+
+    const response = await fetch("/openai/generateImage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt, size }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("The image could not be generated");
+    }
+
+    const data = await response.json();
+    removeSpinner();
+
+    console.log(data);
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function removeSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
