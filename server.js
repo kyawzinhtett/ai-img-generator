8d6805976fe3dd3058f684f1e0a6cf8f361cc7e3
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2023-02-02 22:28:04 +0630

    setup route & controller

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const port = process.env.PORT || 3000;
 // Serve static file
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/openai", require("./routes/openaiRoute"));
+
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
